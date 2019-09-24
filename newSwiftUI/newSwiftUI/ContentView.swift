9958a0395e32aeb560313c9f17cc5b9cb9c9d2fa
Kelby Mittan <kelbymittan@Kelbys-MacBook-Pro.local>
2019-09-24 16:14:42 -0400

    Changed hello world

---
+++ newSwiftUI/newSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  newSwiftUI
+//
+//  Created by Kelby Mittan on 9/24/19.
+//  Copyright © 2019 Kelby Mittan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Changes Made")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
